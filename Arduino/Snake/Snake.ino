41aed90c5f1832343ecbf7dffc0cec6a9c8363ab
JosueC23 <71203742+JosueC23@users.noreply.github.com>
2023-08-08 22:24:18 -0600

    Movimiento Snake
    Se mueve de toda las direcciones arriba, abajo, derecha y izquierda

---
+++ Arduino/Snake/Snake.ino
@@ -0,0 +1,46 @@
+#include "LedControl.h"
+#include "global/global.h"
+#include "funciones/matriz.h"
+#include "clases/criatura.h"
+
+criatura SNAKE;
+
+#include "funciones/botones.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  matriz_driver.shutdown(0, false);
+  matriz_driver.setIntensity(0, 15);
+  matriz_driver.clearDisplay(0);
+
+  matriz_driver.shutdown(1, false);
+  matriz_driver.setIntensity(1, 15);
+  matriz_driver.clearDisplay(1);
+  
+  pinMode(BTN_IZQ,INPUT);
+  pinMode(BTN_DER,INPUT);
+  pinMode(BTN_ARRIBA, INPUT);
+  pinMode(BTN_ABAJO,INPUT);
+
+  ESTADO = JUEGO;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switch (ESTADO)
+  {
+  case JUEGO:
+    limparMatriz();
+    SNAKE.GenerarCriatura(matriz_pantalla);
+    if (SNAKE.Velocidad())
+    {
+      SNAKE.Movimientos();
+    }
+    pintarMatriz();
+    Botones_Juego();
+    break;
+  default:
+    break;
+  }
+}
